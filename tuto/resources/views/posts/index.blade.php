6d2a6116c703e2a34162e99f3a021a877a796b9b
abderrahmane <abdeja24@gmail.com>
2024-06-25 04:32:07 +0100

    third commit

--- tuto/resources/views/posts/index.blade.php
+++ tuto/resources/views/posts/index.blade.php
@@ -26,7 +26,7 @@
               <td>             
                   <a href="{{route('posts.show', $post['id'])}}" class="btn btn-info">View</a>
                   <a href="{{route('posts.edit', $post['id'])}}" class="btn btn-primary">Edit</a>
-                  <form action="{{route('posts.destroy', $post['id'])}}" method="POST">
+                  <form style="display: inline" action="{{route('posts.destroy', $post['id'])}}" method="POST">
                     @csrf
                     @method('delete')
                     <button type="submit" class="btn btn-danger">Delete</button>
